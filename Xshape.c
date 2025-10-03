ad7597be29a332057eda16fa2ce9525ccfedb206
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 10:58:40 +0530

    Add files via upload

---
+++ Xshape.c
@@ -0,0 +1,25 @@
+//*     *
+// *   *
+//  * *
+//   *
+//  * *
+// *   *
+//*     *
+//print row or column
+//adding space and * into my print command   
+//* *
+// *
+//* *
+#include <stdio.h>
+int main()
+{
+    printf("*     *\n");
+    printf(" *   *\n");
+    printf("  * *\n");
+    printf("   *\n");
+    printf("  * *\n");
+    printf(" *   *\n");
+    printf("*     *\n");
+
+    return 0;
+}
