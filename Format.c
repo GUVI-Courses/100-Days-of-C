919758c82716ace8f0f2712f45d0a216279b5b90
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 10:54:11 +0530

    Add files via upload

---
+++ Format.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("My age is 26\n");
+    printf("My age is %d\n",26);
+    printf("My age is currently %d and next year i will be %d\n",26,27);
+    printf("My grade point is %d\n",95.6);
+    printf("My grade point is %f\n",95.6);
+    printf("My grade point is %.2f\n",95.6);
+    return 0 ;
+}
---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    //print addition operation
+    printf("Addition of 2 numbers : %d,%d = %d\n",5,4,5+4);
+    //print substraction
+    printf("Subtraction of 2 numbers : %d,%d = %d\n",10,2,10-2);
+    //print multiplication
+    printf("Multiplication of 2 numbers : %d,%d = %d\n",5,4,5*4);
+    //print division 
+    printf("Division of 2 numbers : %d,%d = %d\n",8,4,8/4);
+    //print remainder
+    printf("remainder of 2 numbers : %d,%d = %d\n",5,4,5%4);
+}
