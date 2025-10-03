9c09f7d904018d78db28e12a414c1c097a72a0e7
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 11:06:30 +0530

    Add files via upload

---
+++ casting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 5;
+    int num2 = 2;
+    double result; // 5/2=2.5
+    result = (double)num1/num2;
+    //5 2 5/2 = 2 ----- result = 2.0000
+    // 3+4.5 = 7.5
+    printf("The result is : %f",result);
+
+    
+}
