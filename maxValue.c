7fc503f9b7a21d13170f5bea97f410f3e3164f72
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 12:22:07 +0530

    Add files via upload

---
+++ maxValue.c
@@ -0,0 +1,31 @@
+//another method you can try
+#include <stdio.h>
+
+// Function to find maximum value in array
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int maximum = findMax(arr, n);
+    printf("Maximum value in the array is: %d\n", maximum);
+
+    return 0;
+}
+
