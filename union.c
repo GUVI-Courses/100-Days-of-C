63d8f2210c9bdd38bf9d97d1e6e8bb6b6df503b0
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 14:53:19 +0530

    Add files via upload

---
+++ union.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+union info
+{
+    char firstName[20];
+    int age;
+};
+
+int main()
+{
+    //template for creating a UNion Variable
+    //union <union_name> <variable_name>;
+
+    union info myVariable1;
+}
