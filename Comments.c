d4fe3cf5d31111040ed4e4b68cd74bb11bcdaa6e
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 10:51:03 +0530

    Add files via upload

---
+++ Comments.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+
+/*printing name age and gender.
+specify my starting and ending point of
+commnet without using this
+double slash */
+printf("Name : Disha Jain\nAge : 26\nGender : Female");
+return 0;
