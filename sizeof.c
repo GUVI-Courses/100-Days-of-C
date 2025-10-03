26b024164f9db66572325ebb73f4aa133e4c11fb
GUVI-Courses <123951681+GUVI-Courses@users.noreply.github.com>
2025-10-03 11:28:09 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x ; 
+    printf("%d\n",sizeof(x));
+    printf("%d\n",sizeof(int));
+    printf("%d\n",sizeof(5));
+    printf("%d\n",sizeof(char));
+    printf("%d\n",sizeof(short int));
+    printf("%d\n",sizeof(float));
+    printf("%d\n",sizeof(double));
+    printf("%d\n",sizeof(long double));
+}
